c1e156014df8337f04e960fca4c32e2ea5acd94b
Thomas Dobson <tom@Thomass-MacBook-Air.local>
2018-11-14 22:17:13 -0800

    Final App

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    var motionManager: CMMotionManager!
+    
     @IBOutlet weak var xLabel: UILabel!
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        motionManager = CMMotionManager()
+        motionManager.accelerometerUpdateInterval = 0.3
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: uppdateLabels)
+    }
+    
+    func uppdateLabels(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else { return }
+        xLabel.text = String(format: "X: %.1f", accelerometerData.acceleration.x)
+        yLabel.text = String(format: "Y: %.1f", accelerometerData.acceleration.y)
+        zLabel.text = String(format: "Z: %.1f", accelerometerData.acceleration.z)
     }
 
 }
