35dfce7f657c266a0ddfafec14cb1ee2dfd5af7e
Thomas Dobson <tom@thomass-macbook-air.local>
2018-11-14 21:15:42 -0800

    Initial Commit

---
+++ AccelerometerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccelerometerDemo
+//
+//  Created by Thomas Dobson on 11/14/18.
+//  Copyright © 2018 Thomas Dobson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
